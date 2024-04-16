0c3da9de9b5530847273f48e8dbfb524fd82c210
Roselyn <113182476+Oreva-tech@users.noreply.github.com>
2024-04-16 08:39:42 +0100

    edited

---
+++ src/quiz/Congratulation.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Congratulation =()=>{
+    return(
+        <>
+        </>
+    )
+}
+
+
+export default Congratulation
---
+++ src/quiz/Instruction.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+const Instruction =()=>{
+    return(
+        <>
+        </>
+    )
+}
+
+
+export default Instruction 
