1d7e8f9e3233c99bfce77f1ec05e4e087b3b27fe
Roselyn <113182476+Oreva-tech@users.noreply.github.com>
2024-04-16 16:54:09 +0100

    Update Instruction.jsx

--- src/pages/quiz/Instruction.jsx
+++ src/pages/quiz/Instruction.jsx
@@ -18,13 +18,12 @@ const Instruction =()=>{
                 information presented in the topic covered in the module.
                 Each question on the quiz is of mulitple choice or true or false format and you have an avearge of 10sec for each question
 
-                </li>
+                 </li>
 
                 <li>On the selection of the response, the next question pop.</li>
 
 
                 <li>Should you be unsure of the question, it is advisable to skip to the next</li>
-
 
             </ul>           
                 
