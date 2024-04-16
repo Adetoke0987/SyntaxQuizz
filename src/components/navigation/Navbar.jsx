d7c888641af8b277cbe57c1bcd85f612d6e90243
Adetoke Durojaiye <awesometokeh@gmail.com>
2024-04-16 17:02:23 +0100

    update

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
@@ -42,3 +43,49 @@ const Navbar = () => {
 };
 
 export default Navbar;
+=======
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar-container">
+      <div className="logo-container">
+        <Link to="/">
+          <img src="../../src/assets/syntax.png" alt="logo" className="logo" />
+        </Link>
+      </div>
+      <div className="search">
+        <input type="text" placeholder="search" />
+      </div>
+      <div className="links-container">
+        <Link to="/" className="nav-link">
+          Home
+        </Link>
+        <Link to="/connect" className="nav-link">
+          Connect
+        </Link>
+        <Link to="/pricing" className="nav-link">
+          Pricing
+        </Link>
+        <Link to="/help" className="nav-link">
+          Help
+        </Link>
+        <Link to="/login" className="nav-link">
+          Login
+        </Link>
+        <Link to="/signup" className="nav-link">
+          Sign up
+        </Link>
+        <i className="fa fa-search search-icon"></i>
+        <Link to="/signup" className="create-button">
+          Create Quiz
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+>>>>>>> Stashed changes
