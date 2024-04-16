44378f83e99f74a28811ecdb755e3435043fbff9
Roselyn <113182476+Oreva-tech@users.noreply.github.com>
2024-04-16 15:20:43 +0100

    Update Congratulation.jsx

--- src/pages/quiz/Congratulation.jsx
+++ src/pages/quiz/Congratulation.jsx
@@ -4,6 +4,20 @@ import React from "react";
 const Congratulation =()=>{
     return(
         <>
+        <div>
+            <h2>Category:</h2>
+            <h4>Number of question</h4>
+            
+            <h3>Where is located</h3>
+
+            <span>
+                <h5><input type ="checkbox"/>wwwwwwwwwwfdtdh</h5>
+                <h5><input type ="checkbox"/>wwwwwwwwwwfdtdh</h5>
+                <h5><input type ="checkbox"/>wwwwwwwwwwfdtdh</h5>
+                <h5><input type ="checkbox"/>wwwwwwwwwwfdtdh</h5>
+            </span>
+
+        </div>
         </>
     )
 }
