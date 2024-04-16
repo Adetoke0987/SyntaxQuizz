d2c439a4d296d56e417bc5dba46417cfa97b184d
Roselyn <113182476+Oreva-tech@users.noreply.github.com>
2024-04-16 09:11:00 +0100

    edited

---
+++ src/pages/quiz/Instruction.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+
+const Instruction =()=>{
+    return(
+        <div>
+
+
+            <div>
+                <h1>Category:</h1>
+            </div>
+
+            
+        </div>
+    )
+}
+
+
+export default Instruction 
