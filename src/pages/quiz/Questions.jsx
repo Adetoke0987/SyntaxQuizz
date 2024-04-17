569faf2fef3ee91c56855b651807a9ef13ea3c98
Akintomiwa Peter Adedokun <143067882+Akintomiwa200@users.noreply.github.com>
2024-04-17 08:41:32 +0100

    update

--- src/pages/quiz/Questions.jsx
+++ src/pages/quiz/Questions.jsx
@@ -4,6 +4,22 @@ import React from "react"
 const Questions =()=>{
     return(
         <>
+            <div id="main">
+                <div>
+                    <h1>{Questions.category}</h1>
+                    <h3>question  no | no of question</h3>
+                    <p>who is presdent of nigeria</p>
+                    
+                    <div id="answers">
+                        <span id="ans">option a</span>
+                        <span id="ans">option b</span>
+                        <span id="ans">option c</span>
+                        <span id="ans">option d</span>
+                    </div>
+
+
+                </div>
+            </div>
         </>
     )
 }
--- src/pages/quiz/Quiz.jsx
+++ src/pages/quiz/Quiz.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Quiz = () => {
   return (
-    <div>Quiz</div>
-  )
-}
+    <div>
+      <h1>Quiz Module</h1>
+      <Link to="/questions">
+        <button>Start Quiz</button>
+      </Link>
+    </div>
+  );
+};
 
-export default Quiz
+export default Quiz;
