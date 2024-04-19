bd56a9a425d172b9360e8f60f9ab731d2fbc8aa4
Akintomiwa Peter Adedokun <143067882+Akintomiwa200@users.noreply.github.com>
2024-04-19 03:48:04 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,32 +17,32 @@ const Home = () => {
       <div className="teams">
         <h2>Meet the Team</h2>
         <div>
-        <div class="profilecard">
+        <div className="profilecard">
       <img src="person1.jpg" alt="Person 1 Profile Picture"/>
       <h2>John Doe</h2>
       <p>Software Engineer</p>
     </div>
-    <div class="profilecard">
+    <div className="profilecard">
       <img src="person2.jpg" alt="Person 2 Profile Picture"/>
       <h2>Jane Smith</h2>
       <p>Marketing Manager</p>
     </div>
-    <div class="profilecard">
+    <div className="profilecard">
       <img src="person3.jpg" alt="Person 3 Profile Picture"/>
       <h2>Alice Lee</h2>
       <p>Graphic Designer</p>
     </div>
-    <div class="profilecard">
+    <div className="profilecard">
       <img src="person1.jpg" alt="Person 1 Profile Picture"/>
       <h2>John Doe</h2>
       <p>Software Engineer</p>
     </div>
-    <div class="profilecard">
+    <div className="profilecard">
       <img src="person2.jpg" alt="Person 2 Profile Picture"/>
       <h2>Jane Smith</h2>
       <p>Marketing Manager</p>
     </div>
-    <div class="profilecard">
+    <div className="profilecard">
       <img src="person3.jpg" alt="Person 3 Profile Picture"/>
       <h2>Alice Lee</h2>
       <p>Graphic Designer</p>
