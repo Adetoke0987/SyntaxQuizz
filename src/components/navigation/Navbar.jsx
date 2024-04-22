64e96eb684a3c6b0f37afed4c4cdd7927bb483cf
Nimderal <107073424+Nimderal@users.noreply.github.com>
2024-04-22 17:18:53 +0100

    UPDATE

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -2,57 +2,60 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
-import {FcMenu} from 'react-icons/fc';
-import {IoShareOutline} from 'react-icons/io5';
-import {IoMdClose} from 'react-icons/io';
+import { FcMenu } from 'react-icons/fc';
+import { IoShareOutline } from 'react-icons/io5';
+import { IoMdClose } from 'react-icons/io';
 import syntax from '../../../src/assets/syntax.png'
 
 const Navbar = () => {
-  
-    const [isOpen, setIsOpen]= useState(false);
 
-    const toggleMenu = ()=>{
-      const data = !isOpen
-      setIsOpen(data);
-    };
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    const data = !isOpen
+    setIsOpen(data);
+  };
 
 
   return (
     <nav className="navbar-container">
-      <div onClick={toggleMenu}>
-        {isOpen ? <IoMdClose/> : <FcMenu/> }
+      <div className="text2">
+        <div onClick={toggleMenu} className="text">
+          {isOpen ? <IoMdClose /> : <FcMenu />}
         </div>
         {isOpen && (
-         <div className="links-container">
-          <NavLink to="/" className="nav-link">
-          Home
-        </NavLink>
-        <NavLink to="/help" className="nav-link">
-          Help
-        </NavLink>
-        <NavLink to="/login" className="nav-link">
-          Login
-        </NavLink>
-        <NavLink to="/signup" className="nav-link">
-          Sign up
-        </NavLink>
-        <i className="fa fa-search search-icon"></i>
-        <NavLink to="/signup" className="create-button">
-          Create Quiz
-        </NavLink> 
-        
-      </div>
-        
+          <div className="links-container">
+            <NavLink to="/" className="nav-link">
+              Home
+            </NavLink>
+            <NavLink to="/help" className="nav-link">
+              Help
+            </NavLink>
+            <NavLink to="/login" className="nav-link">
+              Login
+            </NavLink>
+            <NavLink to="/signup" className="nav-link">
+              Sign up
+            </NavLink>
+            <i className="fa fa-search search-icon"></i>
+            <NavLink to="/signup" className="create-button">
+              Create Quiz
+            </NavLink>
+
+          </div>
+
         )}
-      
-        <div className="logo-container">
-        <NavLink to="/">
+      </div>
+
+
+      <div className="logo-container">
+        {/* <NavLink to="/"> */}
           <img src={syntax} alt="logo" className="logo" />
-        </NavLink>
+        {/* </NavLink> */}
       </div>
 
       <div>
-        <IoShareOutline/>
+        <IoShareOutline />
       </div>
     </nav>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
+import { IoIosArrowForward } from "react-icons/io";
 
 const Home = () => {
   return (
@@ -25,46 +26,46 @@ const Home = () => {
         <h2>Meet the Team</h2>
         <div className="grid">
           <div className="profilecard">
-            <img src="person1.jpg" alt="Person 1 Profile Picture" />
+            <img src="https://www.google.com/imgres?q=adedokun%20peter%20akintomiwa&imgurl=https%3A%2F%2Fpbs.twimg.com%2Fmedia%2FF4oYHaeXMAAVYsV%3Fformat%3Djpg%26name%3Dlarge&imgrefurl=https%3A%2F%2Ftwitter.com%2Fherkintormiwer&docid=neL-p75rF5XLuM&tbnid=u_ep0lkfUiQYHM&vet=12ahUKEwjZyoC8mtaFAxU6WUEAHeCEBtsQM3oECBAQAA..i&w=960&h=1280&hcb=2&itg=1&ved=2ahUKEwjZyoC8mtaFAxU6WUEAHeCEBtsQM3oECBAQAA" alt="Person 1 Profile Picture" />
             <h2>Adedokun Peter Akintomiwa</h2>
             <p>Software Engineer</p>
-            <a href="https://akintomiwa-portfolio.vercel.app"></a>
+            <a href="https://akintomiwa-portfolio.vercel.app"><IoIosArrowForward /></a>
           </div>
           <div className="profilecard">
             <img src="person1.jpg" alt="Person 1 Profile Picture" />
             <h2>Yusuf Olatunji</h2>
             <p>Software Engineer</p>
-            <a href="https://akintomiwa-portfolio.vercel.app"></a>
+            <a href="https://akintomiwa-portfolio.vercel.app"><IoIosArrowForward /></a>
           </div>
           <div className="profilecard">
             <img src="person2.jpg" alt="Person 2 Profile Picture" />
             <h2>Roselyn </h2>
             <p>Marketing Manager</p>
-            <a href="https://akintomiwa-portfolio.vercel.app"></a>
+            <a href="https://akintomiwa-portfolio.vercel.app"><IoIosArrowForward /></a>
           </div>
           <div className="profilecard">
             <img src="person3.jpg" alt="Person 3 Profile Picture" />
             <h2>David</h2>
             <p>Graphic Designer</p>
-            <a href="https://akintomiwa-portfolio.vercel.app"></a>
+            <a href="https://akintomiwa-portfolio.vercel.app"><IoIosArrowForward /></a>
           </div>
           <div className="profilecard">
             <img src="person1.jpg" alt="Person 1 Profile Picture" />
             <h2>Adetoke</h2>
             <p>Software Engineer</p>
-            <a href="https://akintomiwa-portfolio.vercel.app"></a>
+            <a href="https://akintomiwa-portfolio.vercel.app"><IoIosArrowForward /></a>
           </div>
           <div className="profilecard">
             <img src="person2.jpg" alt="Person 2 Profile Picture" />
             <h2>Helen Alabi</h2>
             <p>Marketing Manager</p>
-            <a href="https://akintomiwa-portfolio.vercel.app"></a>
+            <a href="https://akintomiwa-portfolio.vercel.app"><IoIosArrowForward /></a>
           </div>
           <div className="profilecard">
             <img src="person3.jpg" alt="Person 3 Profile Picture" />
             <h2>Gbadebo Oyewunmi Nimot</h2>
             <p>Graphic Designer</p>
-            <a href="https://akintomiwa-portfolio.vercel.app"></a>
+            <a href="https://akintomiwa-portfolio.vercel.app"><IoIosArrowForward /></a>
           </div>
         </div>
       </div>
