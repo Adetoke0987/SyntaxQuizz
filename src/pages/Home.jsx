f9de23583725c2743b9061c85f3641892054f51a
Akintomiwa200 <143067882+Akintomiwa200@users.noreply.github.com>
2024-04-23 11:19:35 +0100

    create Profile.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
-import { IoIosArrowForward } from "react-icons/io";
-import akintomiwa from "../../src/assets/Akintomiwa.jpg"
+
+import Profile from "../components/Profile";
 
 const Home = () => {
   return (
@@ -26,49 +26,7 @@ const Home = () => {
       <div className="teams">
         <h2>Meet the Team</h2>
         <div className="grid">
-          <div className="profilecard">
-            <img src={akintomiwa} alt="Person 1 Profile Picture" />
-            <h2>Adedokun Peter Akintomiwa</h2>
-            <p>Software Engineer</p>
-            <a href="https://akintomiwa-portfolio.vercel.app"><IoIosArrowForward /></a>
-          </div>
-          <div className="profilecard">
-            <img src="person1.jpg" alt="Person 1 Profile Picture" />
-            <h2>Yusuf Olatunji</h2>
-            <p>Software Engineer</p>
-            <a href="https://akintomiwa-portfolio.vercel.app"><IoIosArrowForward /></a>
-          </div>
-          <div className="profilecard">
-            <img src="person2.jpg" alt="Person 2 Profile Picture" />
-            <h2>Roselyn </h2>
-            <p>Marketing Manager</p>
-            <a href="https://akintomiwa-portfolio.vercel.app"><IoIosArrowForward /></a>
-          </div>
-          <div className="profilecard">
-            <img src="person3.jpg" alt="Person 3 Profile Picture" />
-            <h2>David</h2>
-            <p>Graphic Designer</p>
-            <a href="https://akintomiwa-portfolio.vercel.app"><IoIosArrowForward /></a>
-          </div>
-          <div className="profilecard">
-            <img src="person1.jpg" alt="Person 1 Profile Picture" />
-            <h2>Adetoke</h2>
-            <p>Software Engineer</p>
-            <a href="https://akintomiwa-portfolio.vercel.app"><IoIosArrowForward /></a>
-          </div>
-          <div className="profilecard">
-            <img src="person2.jpg" alt="Person 2 Profile Picture" />
-            <h2>Helen Alabi</h2>
-            <p>Marketing Manager</p>
-            <a href="https://akintomiwa-portfolio.vercel.app"><IoIosArrowForward /></a>
-          </div>
-          <div className="profilecard">
-            <img src="person3.jpg" alt="Person 3 Profile Picture" />
-            <h2>Gbadebo Oyewunmi Nimot</h2>
-            <p>Graphic Designer</p>
-            <a href="https://akintomiwa-portfolio.vercel.app"><IoIosArrowForward /></a>
-          </div>
-        </div>
+          <Profile/>
       </div>
 
       <div className="footer">
