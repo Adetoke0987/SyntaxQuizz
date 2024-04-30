748dc07af88121bd463e0688093c124c21bd3c9c
Akintomiwa200 <143067882+Akintomiwa200@users.noreply.github.com>
2024-04-30 09:28:21 +0100

    reg

--- src/components/loginComponents/LoginComponent.jsx
+++ src/components/loginComponents/LoginComponent.jsx
@@ -1,94 +1,57 @@
-import React, { useContext, useState } from "react";
-import Inputs from "./Inputs";
-// import Dashboard from '../../pages/dashboard/Dashboard'; // Import Dashboard component
-import { userData } from "./UserContext";
-import { Link } from "react-router-dom/dist";
-import style from "./login.module.css";
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const LoginComponent = () => {
-  const { users } = useContext(userData);
-
-  const { fullname, password } = users;
-  //The Context that will be available on all pages is the users which contains the user detils
-
-  const [userDetails, setUserDetails] = useState({
-    fullname: "",
-    email: "",
-    password: "",
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
   });
 
-  //A function to handle the changes observed on the input feilds
-  const handleInputChange = (e) => {
+  const handleChange = (e) => {
     const { name, value } = e.target;
-    setUserDetails({
-      ...userDetails,
+    setFormData((prev) => ({
+      ...prev,
       [name]: value,
-    });
+    }));
   };
 
-  const addUsers = (e) => {
+  const submitHandler = async (e) => {
     e.preventDefault();
-    updateUser([...users, userDetails]);
-    setUserDetails({
-      fullname: "",
-      email: "",
-      password: "",
-    });
-
-    console.log(users);
+    console.log(formData);
+    try {
+      const response = await axios.post(
+        'https://questionsapi.onrender.com/auth/users',
+        formData
+      );
+      const token = await response.data.data.token;
+      localStorage.setItem('token', token);
+      console.log('Sign In successful:', response.data.data.token);
+      useNavigate('/home');
+    } catch (error) {
+      console.error('Signup failed:', error);
+    }
   };
-
-  const logInInputs = [
-    {
-      labelText: "Email",
-      inputType: "email",
-      placeholderText: "Enter Your Email",
-      inputName: "fullname",
-      inputValue: fullname,
-    },
-    {
-      labelText: "Password",
-      inputType: "password",
-      placeholderText: "Password",
-      inputName: "passwod",
-      inputValue: password,
-    },
-  ];
-
   return (
-    <div className={style.main}>
-      <form onSubmit={addUsers} className={style.loginform}>
-        {logInInputs.map(
-          ({
-            labelText,
-            inputType,
-            inputName,
-            placeholderText,
-            inputValue,
-          }) => {
-            return (
-              <Inputs
-                labelText={labelText}
-                inputType={inputType}
-                inputName={inputName}
-                placeholderText={placeholderText}
-                key={labelText}
-                inputValue={inputValue}
-              />
-            );
-          }
-        )}
-        <div className={style.formSubmit}>
-          <Link to={"/dashboard"}>
-            <button className={style.button}>Log in</button>
-          </Link>
-        </div>
-        <div className={style.not}>
-          <p className={style.p}>Dont have an Account?</p>
-          <Link to={"/signup"} className={style.h2}>Register</Link>
-        </div>
+    <>
+      <form onSubmit={submitHandler}>
+        <input
+          type="email"
+          placeholder="email"
+          onChange={handleChange}
+          name="email"
+          value={formData.email}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          onChange={handleChange}
+          name="password"
+          value={formData.password}
+        />
+        <button type="submit">Log in</button>
       </form>
-    </div>
+    </>
   );
 };
 
--- src/components/loginComponents/RegistrationComponent.jsx
+++ src/components/loginComponents/RegistrationComponent.jsx
@@ -1,115 +1,73 @@
-import React, { useContext, useState } from "react";
-import Inputs from "./Inputs";
-import { userData } from "./UserContext";
-import { Link } from "react-router-dom";
-import style from "./login.module.css";
+import React, { useState } from 'react';
+import './signup.css';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const RegistrationComponent = () => {
-  const { users, updateUser } = useContext(userData);
-  const [userDetails, setUserDetails] = useState({
-    firstname: "",
-    lastname: "",
-    email: "",
-    password: "",
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
   });
-  const [tooltipMessage, setTooltipMessage] = useState(""); // State to manage tooltip message
 
-  const handleInputChange = (e) => {
+  const handleChange = (e) => {
     const { name, value } = e.target;
-    setUserDetails({
-      ...userDetails,
+    setFormData((prev) => ({
+      ...prev,
       [name]: value,
-    });
+    }));
   };
 
-  const addUsers = async (e) => {
+  const submitHandler = async (e) => {
     e.preventDefault();
+    console.log(formData);
     try {
       const response = await axios.post(
         'https://questionsapi.onrender.com/auth/signup',
-        userDetails
+        formData
       );
-      setTooltipMessage("User successfully registered"); // Set success tooltip message
-      updateUser([...users, userDetails]);
-      setUserDetails({
-        firstname: "",
-        lastname: "",
-        email: "",
-        password: "",
-      });
+
+      console.log('Signup successful:', response.data);
+      useNavigate('/login');
     } catch (error) {
-      setTooltipMessage("Invalid user details"); // Set error tooltip message
       console.error('Signup failed:', error.response.data);
     }
   };
-
-  const inputs = [
-    {
-      labelText: "First Name",
-      inputType: "text",
-      placeholderText: "Enter Your First Name",
-      inputName: "firstname",
-      inputValue: userDetails.firstname,
-    },
-    {
-      labelText: "Last Name",
-      inputType: "text",
-      placeholderText: "Enter Your Last Name",
-      inputName: "lastname",
-      inputValue: userDetails.lastname,
-    },
-    {
-      labelText: "Email",
-      inputType: "email",
-      placeholderText: "Enter Your Email",
-      inputName: "email",
-      inputValue: userDetails.email,
-    },
-    {
-      labelText: "Password",
-      inputType: "password",
-      placeholderText: "Enter Your Password",
-      inputName: "password",
-      inputValue: userDetails.password,
-    },
-  ];
-
   return (
-    <div className={style.main}>
-      <form onSubmit={addUsers} className={style.registerform}>
-        <h2 className={style.h2}>Register Your Account</h2>
-        {inputs.map(
-          ({
-            labelText,
-            inputType,
-            inputName,
-            placeholderText,
-            inputValue,
-          }) => {
-            return (
-              <Inputs
-                labelText={labelText}
-                inputType={inputType}
-                inputName={inputName}
-                placeholderText={placeholderText}
-                key={labelText}
-                inputValue={inputValue}
-                inputfunc={handleInputChange}
-              />
-            );
-          }
-        )}
-        <div className={style.formSubmit}>
-          <button type="submit" className={style.button}>Submit</button>
-          {tooltipMessage && <span className={style.tooltip}>{tooltipMessage}</span>}
-        </div>
-        <div className={style.not}>
-          <p className={style.p}>Have an Account?</p>
-          <Link to="/login" className={style.h2}> Login</Link>
-        </div>
+    <>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          placeholder="firstName"
+          onChange={handleChange}
+          name="firstName"
+          value={formData.firstName}
+        />
+        <input
+          type="text"
+          placeholder="lastName"
+          onChange={handleChange}
+          name="lastName"
+          value={formData.lastName}
+        />
+        <input
+          type="email"
+          placeholder="email"
+          onChange={handleChange}
+          name="email"
+          value={formData.email}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          onChange={handleChange}
+          name="password"
+          value={formData.password}
+        />
+        <button type="submit">Sign up</button>
       </form>
-    </div>
+    </>
   );
 };
 
